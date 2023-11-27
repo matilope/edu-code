417dfc9b60cca898e3e344c335cb4ebea72b4635
Matias <mati02171996@gmail.com>
2023-11-27 00:09:24 -0300

    feat: router guards and user profile view

--- src/router/index.js
+++ src/router/index.js
@@ -25,42 +25,42 @@ const routes = [
   {
     path: '/chat',
     component: () => import('../pages/Chat.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "user" }
   },
   {
     path: '/perfil',
     component: () => import('../pages/Profile.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: ["admin", "user"] }
   },
   {
     path: '/usuarios',
     component: () => import('../pages/UserList.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "admin" }
   },
   {
     path: '/usuarios/:id',
     component: () => import('../pages/UserProfile.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "admin" }
   },
   {
     path: '/usuarios/:id/chat',
     component: () => import('../admin/Chat.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "admin" }
   },
   {
     path: '/admin/cursos',
     component: () => import('../admin/Services.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "admin" }
   },
   {
     path: '/admin/cursos/crear',
     component: () => import('../admin/CreateService.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "admin" }
   },
   {
     path: '/admin/cursos/:id/editar',
     component: () => import('../admin/EditService.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, role: "admin" }
   },
   {
     path: '/:catchAll(.*)',
@@ -84,14 +84,12 @@ let user = {
 subscribeToAuth(newUser => user = newUser);
 
 router.beforeEach((to, from) => {
-  if (to.path === "/chat" && user.role === "admin") {
-    return '/perfil';
-  }
-  if (to.path === "/usuarios" && user.role !== "admin") {
-    return '/perfil';
-  }
-  if (user.id === null && to.meta.requiresAuth) {
-    return '/iniciar-sesion';
+  if (to.meta.requiresAuth) {
+    if (user.id === null) {
+      return '/iniciar-sesion';
+    } else if (to.meta.role && user.role !== to.meta.role && !to.meta.role.includes(user.role)) {
+      return '/perfil';
+    }
   }
 });
 
